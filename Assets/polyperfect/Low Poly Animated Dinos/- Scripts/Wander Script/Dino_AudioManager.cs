32c0efac626914bd50ecc66822a64196712c4977
Leona <iosef1004@hotmail.com>
2022-02-18 15:09:09 -0500

    Reorganized project

--- Assets/polyperfect/Low Poly Animated Dinos/- Scripts/Wander Script/Dino_AudioManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Polyperfect.Common;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Polyperfect.Dinos
-{
-    public class Dino_AudioManager : Common_AudioManager { }
-}

4f79903a494ae633cbe5ca26153e5f83a092f493
tygriis <tygr2.115@gmail.com>
2022-02-19 18:54:52 -0500

    pseudocode2

--- Assets/Scripts/Energy/Energy.cs
+++ Assets/Scripts/Energy/Energy.cs
@@ -6,6 +6,8 @@ public class Energy : MonoBehaviour
 {
     public Image fill;
     public TextMeshProUGUI amount;
+    private bool isMoving; //checks if Dino is moving - false means stationary
+    private Transform prevPosition; //stores last position of the Dino
 
     public int currentValue, maxValue;
 
@@ -17,7 +19,43 @@ public class Energy : MonoBehaviour
         // StartCoroutine(AddDelay);
     }
 
+    void Update()
+    {
+        prevPosition = GameObject
+    }
 
+    public void Drain_Energy()
+    {
+        //when 3 seconds pass . . .
+
+        //if player is stationary
+        if(isMoving)
+        //drain x energy
+
+        //else, drain 2x energy                                                                                                 
+
+    }
+
+    private void MoveCheck()
+    {
+        //check if previous position matches current position
+        if 
+    }
+    public void Replenish_Energy()
+    {
+        //when player eats food
+
+        //increase energy by 10
+    }
+    public void Death()
+    {
+        //when energy reaches 0
+
+        //trigger Death animation
+
+        //lock out player, send to sky view
+    }
+    
     public void Add(int val)
     {
         // currentValue = Mathf.Min(currentValue+val,maxValue);
@@ -32,7 +70,7 @@ public class Energy : MonoBehaviour
 
     }
 
-    
+
 
     private float Normalise()
     {

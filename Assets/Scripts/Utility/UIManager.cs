23ae6b22a10c6e068e59bec0320a8d533b683ce8
Leona <iosef1004@hotmail.com>
2022-03-03 21:48:20 -0500

    Out of Energy Menu implemented

--- Assets/Scripts/Utility/UIManager.cs
+++ Assets/Scripts/Utility/UIManager.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public Canvas OutOfEnergy;
+    public Canvas SelectDino;
+    public Canvas EnergyBar;
+
     public void TestButton()
     {
         Debug.Log("Button pressed");
+    }
+
+    public void BackButton()
+    {
+        OutOfEnergy.GetComponent<Canvas>().enabled = false;
+        SelectDino.GetComponent<Canvas>().enabled = false;
     }
 }
